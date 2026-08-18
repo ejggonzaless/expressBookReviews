ba2442593b40b31129f73d30248afcc940fc58cc
ejggonzaless <ejggonzaless@gmail.com>
2026-08-18 08:41:38 -0400

    Complete Task 1

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,29 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Authentication required"
+        });
+    }
+
+    if (!req.session.authorization) {
+        return res.status(401).json({
+            message: "User is not authenticated"
+        })
+    }
+
+    const sessionToken = req.session.authorization.accessToken;
+
+    if (token !== `Bearer ${sessionToken}`) {
+        return res.status(403).json({
+            message: "Invalid access token"
+        })
+    }
+
+    next();
 });
  
 const PORT =5000;
